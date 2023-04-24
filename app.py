babca4e5f0ea23b6d04c51eafec3da7dbd346b9c
Vipul Sharma <58945750+Vipul-183@users.noreply.github.com>
2023-04-25 02:09:30 +0530

    Update

--- app.py
+++ app.py
@@ -2,25 +2,25 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
-# # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-# st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
-# # ---- READ EXCEL ----
-# st.cache_data
-# def get_data_from_excel():
-#     df = pd.read_excel(
-#         io="supermarkt_sales.xlsx",
-#         engine="openpyxl",
-#         sheet_name="Sales",
-#         skiprows=3,
-#         usecols="B:R",
-#         nrows=1000,
-#     )
-#     # Add 'hour' column to dataframe
-#     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
-#     return df
+# ---- READ EXCEL ----
+st.cache_data
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="supermarkt_sales.xlsx",
+        engine="openpyxl",
+        sheet_name="Sales",
+        skiprows=3,
+        usecols="B:R",
+        nrows=1000,
+    )
+    # Add 'hour' column to dataframe
+    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
 
-# df = get_data_from_excel()
+df = get_data_from_excel()
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
@@ -118,24 +118,3 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
-
-# ---- READ EXCEL ----
-st.cache_data
-def get_data_from_excel():
-    df = pd.read_excel(
-        io="supermarkt_sales.xlsx",
-        engine="openpyxl",
-        sheet_name="Sales",
-        skiprows=3,
-        usecols="B:R",
-        nrows=1000,
-    )
-    # Add 'hour' column to dataframe
-    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
-    return df
-
-df = get_data_from_excel()
