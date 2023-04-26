96dc33d774e87acb0645939aac4377e76d6c0772
Nikhil-Raj-B19CSE059 <raj.14@iitj.ac.in>
2023-04-26 15:08:55 +0530

    UI changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Sales Dashboard", page_icon=":chart_increasing_with_yen:", layout="wide")
 
 # ---- READ EXCEL ----
 st.cache_data
